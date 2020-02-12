b83092c9d1bdc8e0ca3ec34381b7be325fe432b1
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-12 21:49:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,8 +48,9 @@ def self.create(name, grade)
 end
 
 def self.new_from_db(row)
+    @id = row[0]
   new_student = self.new(row[1], row[2])
-  @id = row[0]
+
   new_student.id = row[0]
   new_student.name = row[1]
   new_student.grade = row[2]
