f002a7a366e1fcbbd62a70316a593819714d74dc
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-12 21:21:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,6 @@ class Student
   def update
      sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
      DB[:conn].execute(sql, self.name, self.grade, self.name)
-   end
- end
+end
 
 end
