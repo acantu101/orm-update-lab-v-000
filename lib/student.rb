1fc3f46eea49bee53dc5091be9d4b3b96b700b8f
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-12 21:23:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,18 @@
 require_relative "../config/environment.rb"
 
 class Student
+attr_accessor :name, :grade
+attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
+def initialize(name, grade)
+  @name = name
+  @grade = grade
+  @id = id
+end
+
+ 
+
+    # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def update
      sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
