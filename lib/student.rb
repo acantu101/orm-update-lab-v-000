b77853ed0ffb828dd561f1ca78e1510610103543
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-12 21:38:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,8 +72,8 @@ end
     # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def update
-     sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
-     DB[:conn].execute(sql, self.name, self.grade, self.name)
+     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+     DB[:conn].execute(sql, self.name, self.grade, self.id)
 end
 
 end
