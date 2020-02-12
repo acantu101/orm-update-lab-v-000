2922d371e5981d35255c51853f8c0c2ea732241d
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-12 21:44:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,11 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
-
+def initialize(name,grade)
+  @name = name
+  @grade = grade
+  @id = id
+end
 
 def self.create_table
   sql = <<-SQL
